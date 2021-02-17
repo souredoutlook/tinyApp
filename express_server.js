843f8f93296c1469f45d884cc5ff558eca1ed148
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-17 03:50:37 +0000

    created modules/helper and required generateRandomString, randomQuote there as express_server was getting bloated, add users object for user registration

--- express_server.js
+++ express_server.js
@@ -6,47 +6,30 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.set('view engine', 'ejs');
+
+const { generateRandomString, randomQuote } = require('./modules/helper');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-const generateRandomString = function() {
-  let randomNumberArray = [];
-  do {
-    for (let i = 0; i < 6; i++) {
-      randomNumberArray.push(Math.floor(Math.random() * 62));
-    }
-  } while (urlDatabase[randomNumberArray.join('')] !== undefined)
-  return randomNumberArray.map((element)=>{
-    if (element >= 0 && element <= 9){
-      return String.fromCharCode(element + 48); //0-9 gives numbers 0-9
-    } else if (element >= 10 && element <= 35) {
-      return String.fromCharCode(element + 55); //10-35 gives A-Z
-    } else { // 36 - 61 gives a-z
-      return String.fromCharCode(element + 61)
-    }
-  }).join('');
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
-
-const randomQuote = function() {
-  const quotes = [
-    `"So short I can't believe the ticker symbol isn't GME"`,
-    `"The URLs are shortening so be sure to add flour and salt"`,
-    `"Error 604: Must be this short to ride"`,
-    `"URLs so short they're looking up"`,
-    `"Get Shorty"`,
-    `"Here for a good time (not a long time)"`,
-    `"I'll have a short one"`
-  ]
-  return quotes[Math.floor(Math.random() * quotes.length)]
-};
-
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.redirect(301,'/urls');
 });
 
 app.get('/urls', (req, res) => {
@@ -57,31 +40,31 @@ app.get('/urls', (req, res) => {
     username : name
   };
   res.render("urls_index", templateVars);
-})
+});
 
 app.get('/urls/new', (req, res) => {
   let name = req.cookies !== undefined ? req.cookies["username"] : undefined;
-  const templateVars = { username : name }
+  const templateVars = { username : name };
   res.render("urls_new", templateVars);
 });
 
 
 app.post('/urls', (req, res) => {
-  let randomString = generateRandomString()
+  let randomString = generateRandomString(urlDatabase);
   urlDatabase[randomString] = req.body.longURL;
-  res.redirect(301, `/urls/${randomString}`)
+  res.redirect(301, `/urls/${randomString}`);
 });
 
 app.post('/urls/:shortURL/delete', (req, res)=>{
   let key = req.params.shortURL;
   delete urlDatabase[key];
-  res.redirect(301, '/urls')
+  res.redirect(301, '/urls');
 });
 
 app.post('/urls/:shortURL', (req, res) => {
   let key = req.params.shortURL;
   urlDatabase[key] = req.body.newURL;
-  res.redirect(301, `/urls/${key}`)
+  res.redirect(301, `/urls/${key}`);
 });
 
 app.post('/login',(req, res) => {
@@ -96,7 +79,7 @@ app.post('/logout',(req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let name = req.cookies !== undefined ? req.cookies["username"] : undefined
+  let name = req.cookies !== undefined ? req.cookies["username"] : undefined;
   const templateVars = {
     'shortURL' : req.params.shortURL,
     'longURL' : urlDatabase[req.params.shortURL],
@@ -106,7 +89,7 @@ app.get('/urls/:shortURL', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-  let name = req.cookies !== undefined ? req.cookies["username"] : undefined
+  let name = req.cookies !== undefined ? req.cookies["username"] : undefined;
   const templateVars = { username : name };
   res.render("register", templateVars);
 });
---
+++ modules/helper.js
@@ -0,0 +1,33 @@
+const generateRandomString = function(database) {
+  let randomNumberArray = [];
+  do {
+    for (let i = 0; i < 6; i++) {
+      randomNumberArray.push(Math.floor(Math.random() * 62));
+    }
+  } while (database[randomNumberArray.join('')] !== undefined);
+  return randomNumberArray.map((element)=>{
+    if (element >= 0 && element <= 9) {
+      return String.fromCharCode(element + 48); //0-9 gives numbers 0-9
+    } else if (element >= 10 && element <= 35) {
+      return String.fromCharCode(element + 55); //10-35 gives A-Z
+    } else { // 36 - 61 gives a-z
+      return String.fromCharCode(element + 61);
+    }
+  }).join('');
+};
+
+const randomQuote = function() {
+  const quotes = [
+    `"So short I can't believe the ticker symbol isn't GME"`,
+    `"The URLs are shortening so be sure to add flour and salt"`,
+    `"Error 604: Must be this short to ride"`,
+    `"URLs so short they're looking up"`,
+    `"Get Shorty"`,
+    `"Here for a good time (not a long time)"`,
+    `"I'll have a short one"`,
+    `"Go shorty, it's your birthday, we gon' party like it's your birthday"`
+  ];
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
+
+module.exports = { generateRandomString, randomQuote };
